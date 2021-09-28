fbbe7a1007907942e9c5da1f402cf57c9530aa0f
kksumm123 <kksumm123@gmail.com>
2021-09-28 20:01:23 +0900

    1. MenuUI추가, PlayerInfoUI추가 2. 간단한 버튼로직 추가

---
+++ Assets/MenuUI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class MenuUI : BaseUI<MenuUI>
+{
+    new void Awake()
+    {
+        base.Awake();
+        transform.Find("PlayerInfoButton")
+            .GetComponent<Button>().onClick
+            .AddListener(() => OnClickPlayerInfoButton());
+    }
+
+    private void OnClickPlayerInfoButton()
+    {
+        PlayerInfoUI.Instance.ShowUI();
+    }
+}
