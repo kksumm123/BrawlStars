2f7a1ece8847b196fd86f09c3e628914c2fe1276
kksumm123 <kksumm123@gmail.com>
2021-09-28 15:58:13 +0900

    Initial commit

---
+++ Assets/FindUserScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FindUserScene : MonoBehaviour
+{
+    Text searchResultText;
+    public float findUserTime = 3;
+    IEnumerator Start()
+    {
+        searchResultText = GameObject.Find("Canvas/FindUserScene/SearchResultText").GetComponent<Text>();
+
+        //3초 동안 1 ~ 10으로 까지 숫자를 올린 다음
+        // 로딩 UI를 불러오고 FindUserUI는 꺼지게 해라.
+
+        float startTime = Time.time;
+        float endTime = Time.time + findUserTime;
+        while (Time.time < endTime)
+        {
+            float passedTime = Time.time - startTime;
+            int findUser = Mathf.RoundToInt(Mathf.Lerp(0, 10, passedTime / 3));
+            searchResultText.text = $"찾은 플레이어:{findUser}/10";
+            yield return null;
+        }
+
+        Debug.Log("Loading UI표시하자.");
+        loadingUIs.ForEach(x => x.SetActive(true));
+        gameObject.SetActive(false);
+    }
+    public List<GameObject> loadingUIs;
+}
---
+++ Assets/RawImageScroll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RawImageScroll : MonoBehaviour
+{
+    private RawImage rawImage;
+
+    float curX = 0;
+    float curY = 0;
+    public Vector2 amount;
+
+    private void Awake()
+    {
+        rawImage = GetComponent<RawImage>();
+    }
+
+    void Update()
+    {
+        curX += amount.x;
+        curY += amount.y;
+        rawImage.uvRect = new Rect(new Vector2(curX, curY), rawImage.uvRect.size);
+    }
+}
