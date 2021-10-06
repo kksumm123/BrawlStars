7d8da0a90899435e4343ba3d8925f40a33e7ca3d
kksumm123 <kksumm123@gmail.com>
2021-10-06 18:14:30 +0900

    1. 스크롤 위치에 따라 버튼 하이라이팅 되도록 2. 버튼 누를 시 스크롤좌표계산로직 수정

--- Assets/GameModeSelectUI.cs
+++ Assets/GameModeSelectUI.cs
@@ -18,12 +18,109 @@ public class GameModeSelectUI : MonoBehaviour
             int index = i;
             var item = downMenuButtons[index];
             item.AddListener(this, () => OnClickDownMenu(index));
+        }
 
+        scrollRect.onValueChanged.AddListener(OnGameModeScrollValueChanged);
+
+
+        SetAactiveMenuText(0);
+    }
+
+    private void OnGameModeScrollValueChanged(Vector2 scrollPos)
+    {
+        float x = scrollPos.x;
+
+        int selectedMenuIndex = 0;
+
+        for (int i = downMenuButtons.Length - 1; i >= 0; i--)
+        {
+            float value = (float)i / downMenuButtons.Length;
+            if (value < x)
+            {
+                selectedMenuIndex = i;
+                break;
+            }
+        }
+
+        SetAactiveMenuText(selectedMenuIndex);
+    }
+
+    int lastSelectedIndex;
+    public Color selectedTitleColor = Color.yellow;
+    private void SetAactiveMenuText(int selectedMenuIndex)
+    {
+        downMenuButtons[lastSelectedIndex].GetComponentInChildren<Text>().color = Color.white;
+        downMenuButtons[selectedMenuIndex].GetComponentInChildren<Text>().color = selectedTitleColor;
+        lastSelectedIndex = selectedMenuIndex;
+    }
+
+    float childIndexPosX;
+    bool normalMovable = false;
+    float normalValue;
+    bool scrollMovable = false;
+    void OnClickDownMenu(int index)
+    {
+        scrollMovable = false;
+        normalMovable = false;
+
+        if (scrollRect.content.GetChild(index) != null)
+        {
+            childIndexPosX = scrollRect.GetComponent<RectTransform>().sizeDelta.x * 0.5f
+                - scrollRect.content.GetChild(index).localPosition.x;
+
+            if (index == 0)
+            {
+                normalMovable = true;
+                normalValue = 0;
+
+            }
+            else if (index == scrollRect.content.childCount - 1)
+            {
+                normalMovable = true;
+                normalValue = 1;
+            }
+            else
+            {
+                scrollMovable = true;
+            }
         }
     }
 
-    void OnClickDownMenu(int index)
+    float addedLerpValue;
+    [SerializeField] float lerpValue = 0.1f;
+    void Update()
     {
-        scrollRect.horizontalNormalizedPosition = (float)index / (downMenuButtons.Length - 1);
+        if (scrollMovable)
+        {
+            addedLerpValue += lerpValue * 0.1f;
+
+            var curPosX = scrollRect.content.position.x;
+
+            var distanceX = Mathf.Abs(curPosX) - Mathf.Abs(childIndexPosX);
+            if (Mathf.Abs(distanceX) < 50)
+            {
+                scrollMovable = false;
+            }
+            else
+            {
+                var pos = scrollRect.content.position;
+                pos.x = Mathf.Lerp(curPosX, childIndexPosX, addedLerpValue);
+                scrollRect.content.position = pos;
+            }
+        }
+        else if (normalMovable)
+        {
+            addedLerpValue += lerpValue * 0.1f;
+
+            scrollRect.horizontalNormalizedPosition = Mathf.Lerp(scrollRect.horizontalNormalizedPosition, normalValue, addedLerpValue);
+
+            var distanceX = Mathf.Abs(scrollRect.horizontalNormalizedPosition) - Mathf.Abs(normalValue);
+            if (Mathf.Abs(distanceX) < 0.01f)
+                normalMovable = false;
+        }
+        else
+        {
+            addedLerpValue = 0;
+        }
     }
 }
