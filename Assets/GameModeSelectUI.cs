117d009ce59409483ccfa8d1763619b258030ec7
kksumm123 <kksumm123@gmail.com>
2021-10-06 16:55:04 +0900

    GameModeSelectUI.cs추가 및 초안 작성

---
+++ Assets/GameModeSelectUI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class GameModeSelectUI : MonoBehaviour
+{
+    ScrollRect scrollRect;
+    Button[] downMenuButtons;
+    void Start()
+    {
+        scrollRect = transform.Find("Scroll View").GetComponent<ScrollRect>();
+        downMenuButtons = transform.Find("DownMenu").GetComponentsInChildren<Button>();
+        for (int i = 0; i < downMenuButtons.Length; i++)
+        {
+            int index = i;
+            var item = downMenuButtons[index];
+            item.AddListener(this, () => OnClickDownMenu(index));
+
+        }
+    }
+
+    void OnClickDownMenu(int index)
+    {
+        scrollRect.horizontalNormalizedPosition = (float)index / (downMenuButtons.Length - 1);
+    }
+}
